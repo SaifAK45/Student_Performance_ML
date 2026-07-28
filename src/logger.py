b39d7387e035ff489132f84c655c7e2418229c12
user.name : saif ali khan <saifalikhan8050@gmaol.com>
2026-07-28 20:04:04 +0530

    created a notebook and done with EDA,TRAIN

---
+++ src/logger.py
@@ -0,0 +1,27 @@
+import logging
+import os
+from datetime import datetime
+
+# Create log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Create only the logs directory
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+# Full path to the log file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+#this is for checking the working of logger
+# if __name__ == "__main__":
+#     logging.info("Logger is working successfully!")
+#     print("Logger executed successfully.")
+#     print("Log file created at:", LOG_FILE_PATH)    
